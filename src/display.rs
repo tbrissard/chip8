046bbcc1af7a71d8274d9b82f81abc64ea15a0e2
tbrissard <theo.brissard@outlook.fr>
2026-03-18 11:10:07 +0100

    added screen with sprite writing plus tests

--- src/display.rs
+++ src/display.rs
@@ -1,1 +1,301 @@
+use std::{
+    fmt::Display,
+    ops::{BitAnd, BitXorAssign, ShrAssign},
+};
 
+use num_traits::{Num, WrappingShl, WrappingShr};
+
+#[derive(Debug, Default)]
+pub(crate) struct Sprite {
+    bytes: Vec<u8>,
+}
+
+impl Sprite {
+    const MAX_LEN: usize = 15;
+}
+
+impl From<&[u8]> for Sprite {
+    fn from(value: &[u8]) -> Self {
+        let len = value.len().min(Self::MAX_LEN);
+        Self {
+            bytes: value.iter().copied().take(len).collect(),
+        }
+    }
+}
+
+const DIGIT_0: [u8; 5] = [0xF0, 0x90, 0x90, 0x90, 0xF0];
+const DIGIT_1: [u8; 5] = [0x20, 0x60, 0x20, 0x20, 0x70];
+const DIGIT_2: [u8; 5] = [0xF0, 0x10, 0xF0, 0x80, 0xF0];
+const DIGIT_3: [u8; 5] = [0xF0, 0x10, 0xF0, 0x10, 0xF0];
+const DIGIT_4: [u8; 5] = [0x90, 0x90, 0xF0, 0x10, 0x10];
+const DIGIT_5: [u8; 5] = [0xF0, 0x80, 0xF0, 0x10, 0xF0];
+const DIGIT_6: [u8; 5] = [0xF0, 0x80, 0xF0, 0x90, 0xF0];
+const DIGIT_7: [u8; 5] = [0xF0, 0x10, 0x20, 0x40, 0x40];
+const DIGIT_8: [u8; 5] = [0xF0, 0x90, 0xF0, 0x90, 0xF0];
+const DIGIT_9: [u8; 5] = [0xF0, 0x90, 0xF0, 0x10, 0xF0];
+const DIGIT_A: [u8; 5] = [0xF0, 0x90, 0xF0, 0x90, 0x90];
+const DIGIT_B: [u8; 5] = [0xE0, 0x90, 0xE0, 0x90, 0xE0];
+const DIGIT_C: [u8; 5] = [0xF0, 0x80, 0x80, 0x80, 0xF0];
+const DIGIT_D: [u8; 5] = [0xE0, 0x90, 0x90, 0x90, 0xE0];
+const DIGIT_E: [u8; 5] = [0xF0, 0x80, 0xF0, 0x80, 0xF0];
+const DIGIT_F: [u8; 5] = [0xF0, 0x80, 0xF0, 0x80, 0x80];
+
+pub(crate) type StandardScreen = Screen<u64, 32>;
+
+#[derive(Debug)]
+struct Screen<T, const N: usize> {
+    pixels: [T; N],
+}
+
+impl<T, const N: usize> Screen<T, N>
+where
+    T: Num
+        + Copy
+        + WrappingShl
+        + WrappingShr
+        + BitAnd<Output = T>
+        + From<u8>
+        + BitXorAssign
+        + ShrAssign<i32>,
+{
+    const PIXEL_ON: char = '*';
+    const PIXEL_OFF: char = '_';
+
+    const WIDTH: usize = size_of::<T>() * 8;
+    const HEIGHT: usize = N;
+
+    pub(crate) fn new() -> Self {
+        Self {
+            pixels: [T::zero(); N],
+        }
+    }
+
+    pub(crate) fn clear(&mut self) {
+        for p in &mut self.pixels {
+            *p = T::zero()
+        }
+    }
+
+    fn pixels(&self) -> &[T; N] {
+        &self.pixels
+    }
+
+    /// toggles pixels by XORing the sprite onto the screen, returns true if any pixel was "turned "off"
+    pub(crate) fn write_sprite(&mut self, sprite: &Sprite, mut x: usize, mut y: usize) -> bool {
+        x %= Self::WIDTH;
+        let shift_bits = |byte: T| (byte << (Self::WIDTH - 8)) >> x;
+
+        let mut turned_off = false;
+        y %= Self::HEIGHT;
+        for (sprite_byte, i) in sprite.bytes.iter().copied().zip(y..Self::HEIGHT) {
+            let sprite_byte = shift_bits(sprite_byte.into());
+            let row = self
+                .pixels
+                .get_mut(i)
+                .expect("should never go out of bound");
+            turned_off |= (*row & sprite_byte) != T::zero();
+            *row ^= sprite_byte;
+        }
+
+        turned_off
+    }
+
+    fn line(&self, row: usize) -> String {
+        let mut pixels = String::new();
+        let mut mask = T::one() << (Self::WIDTH - 1);
+
+        for _ in 0..Self::WIDTH {
+            let pixel = if (self.pixels[row] & mask) != T::zero() {
+                Self::PIXEL_ON
+            } else {
+                Self::PIXEL_OFF
+            };
+            pixels.push(pixel);
+            mask >>= 1;
+        }
+
+        pixels
+    }
+
+    fn lines(&self) -> Vec<String> {
+        let mut lines = Vec::new();
+
+        for i in 0..N {
+            lines.push(self.line(i));
+        }
+
+        lines
+    }
+}
+
+impl<T, const N: usize> Display for Screen<T, N>
+where
+    T: Num
+        + Copy
+        + WrappingShl
+        + WrappingShr
+        + BitAnd<Output = T>
+        + From<u8>
+        + BitXorAssign
+        + ShrAssign<i32>,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for l in self.lines() {
+            writeln!(f, "{l}",)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::{DIGIT_0, Sprite, StandardScreen};
+
+    fn create_screen() -> StandardScreen {
+        StandardScreen::new()
+    }
+
+    #[test]
+    fn sprite_from_slice_shorter_than_max_length() {
+        let bytes = vec![1; 1];
+        let sprite = Sprite::from(bytes.as_slice());
+        assert_eq!(sprite.bytes, bytes);
+    }
+
+    #[test]
+    fn sprite_from_slice_equal_to_max_length() {
+        let bytes = vec![1; Sprite::MAX_LEN];
+        let sprite = Sprite::from(bytes.as_slice());
+        assert_eq!(sprite.bytes, bytes);
+    }
+
+    #[test]
+    fn sprite_from_slice_longer_than_max_length() {
+        let bytes = vec![1; Sprite::MAX_LEN + 1];
+        let sprite = Sprite::from(bytes.as_slice());
+        assert_eq!(sprite.bytes, vec![1; Sprite::MAX_LEN]);
+    }
+
+    fn create_sprite() -> Sprite {
+        Sprite::from(DIGIT_0.as_slice())
+    }
+
+    #[test]
+    fn screen_new_empty() {
+        let screen = create_screen();
+        assert!(screen.pixels.iter().all(|row| *row == 0));
+    }
+
+    #[test]
+    fn clear_screen() {
+        let mut screen = create_screen();
+
+        for row in &mut screen.pixels {
+            *row = 0x42;
+        }
+        assert!(screen.pixels.iter().all(|row| *row != 0));
+        screen.clear();
+        assert!(screen.pixels.iter().all(|row| *row == 0));
+    }
+
+    #[test]
+    fn write_sprite_within_bounds() {
+        let mut screen = StandardScreen::new();
+        let sprite = create_sprite();
+
+        let turned_off = screen.write_sprite(&sprite, 0, 0);
+        assert!(!turned_off);
+        #[rustfmt::skip]
+        let pixels: [u64;32] = [
+            0b1111000000000000000000000000000000000000000000000000000000000000,
+            0b1001000000000000000000000000000000000000000000000000000000000000,
+            0b1001000000000000000000000000000000000000000000000000000000000000,
+            0b1001000000000000000000000000000000000000000000000000000000000000,
+            0b1111000000000000000000000000000000000000000000000000000000000000,
+            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        ];
+        assert_eq!(screen.pixels, pixels);
+    }
+
+    #[test]
+    fn write_sprite_cut_horizontally() {
+        let mut screen = StandardScreen::new();
+        let sprite = create_sprite();
+
+        let turned_off = screen.write_sprite(&sprite, 62, 0);
+        assert!(!turned_off);
+        #[rustfmt::skip]
+        let pixels: [u64;32] = [
+            0b0000000000000000000000000000000000000000000000000000000000000011,
+            0b0000000000000000000000000000000000000000000000000000000000000010,
+            0b0000000000000000000000000000000000000000000000000000000000000010,
+            0b0000000000000000000000000000000000000000000000000000000000000010,
+            0b0000000000000000000000000000000000000000000000000000000000000011,
+            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        ];
+        assert_eq!(screen.pixels, pixels);
+    }
+
+    #[test]
+    fn write_sprite_cut_vertically() {
+        let mut screen = StandardScreen::new();
+        let sprite = create_sprite();
+
+        let turned_off = screen.write_sprite(&sprite, 0, 30);
+        assert!(!turned_off);
+        #[rustfmt::skip]
+        let pixels: [u64;32] = [
+            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            0b0000000000000000000000000000000000000000000000000000000000000000,
+            0b0000000000000000000000000000000000000000000000000000000000000000,
+            0b0000000000000000000000000000000000000000000000000000000000000000,
+            0b1111000000000000000000000000000000000000000000000000000000000000,
+            0b1001000000000000000000000000000000000000000000000000000000000000,
+        ];
+        assert_eq!(screen.pixels, pixels);
+    }
+
+    #[test]
+    fn write_sprite_coords_out_bound() {
+        let mut screen = StandardScreen::new();
+        let sprite = create_sprite();
+
+        let turned_off = screen.write_sprite(&sprite, 68, 36);
+        assert!(!turned_off);
+        #[rustfmt::skip]
+        let pixels: [u64;32] = [
+            0,0,0,0,
+            0b0000111100000000000000000000000000000000000000000000000000000000,
+            0b0000100100000000000000000000000000000000000000000000000000000000,
+            0b0000100100000000000000000000000000000000000000000000000000000000,
+            0b0000100100000000000000000000000000000000000000000000000000000000,
+            0b0000111100000000000000000000000000000000000000000000000000000000,
+            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        ];
+        assert_eq!(screen.pixels, pixels);
+    }
+
+    #[test]
+    fn write_sprite_collision() {
+        let mut screen = StandardScreen::new();
+        let sprite_1 = create_sprite();
+        let sprite_2 = create_sprite();
+
+        let turned_off = screen.write_sprite(&sprite_1, 0, 0);
+        assert!(!turned_off);
+        let turned_off = screen.write_sprite(&sprite_2, 3, 0);
+        assert!(turned_off);
+        #[rustfmt::skip]
+        let pixels: [u64;32] = [
+            0b1110111000000000000000000000000000000000000000000000000000000000,
+            0b1000001000000000000000000000000000000000000000000000000000000000,
+            0b1000001000000000000000000000000000000000000000000000000000000000,
+            0b1000001000000000000000000000000000000000000000000000000000000000,
+            0b1110111000000000000000000000000000000000000000000000000000000000,
+            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        ];
+        assert_eq!(screen.pixels, pixels);
+    }
+}
